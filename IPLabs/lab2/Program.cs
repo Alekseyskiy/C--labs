dbe43a00842c81b65593e930171c8348c4e8d481
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-09-29 21:43:52 +0300

    example of game

--- IPLabs/lab2/Program.cs
+++ IPLabs/lab2/Program.cs
@@ -1,10 +1,41 @@
+using System;
+using IPLabs.lab2.model;
+
 namespace IPLabs.lab2
 {
     public class Program
     {
         static void Main()
         {
-            
+            var game = new Game(20);
+
+            string[] commands = new[]
+            {
+                "M 6",
+                "C 17",
+                "P",
+                "C -15",
+                "M -7",
+                "P",
+                "C 3",
+                "C -5",
+                "C 10",
+                "P"
+            };
+
+            foreach (var cmd in commands)
+            {
+                game.ProcessCommandLine(cmd);
+                if (game.IsOver) break; 
+            }
+
+            Console.WriteLine("Print rows:");
+            foreach (var r in game.PrintLines)
+                Console.WriteLine(r);
+
+            Console.WriteLine("\nFinal log:");
+            foreach (var line in game.GetFinalLogLines())
+                Console.WriteLine(line);
         }
     }
 }
