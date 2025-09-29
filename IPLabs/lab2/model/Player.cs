851c1bc80d660b8195b1de4d730831df55d97dd4
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-09-29 21:06:57 +0300

    init Player

--- IPLabs/lab2/model/Player.cs
+++ IPLabs/lab2/model/Player.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Globalization;
+using IPLabs.lab2.enums;
+
 namespace IPLabs.lab2.model
 {
     public class Player
     {
+        public string Name { get; }
+        public int? Position { get; protected set; }
+        public PlayerState State { get; set; }
+        public long DistanceTraveled { get; protected set; }
+
+        public Player(string name)
+        {
+            
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Position = null;
+            State = PlayerState.NotInGame;
+            DistanceTraveled = 0;
+        }
+        
+        public void SetInitialPosition(int pos, int boardSize)
+        {
+            if (boardSize <= 0) throw new ArgumentException("boardSize must be positive");
+            if (pos < 1 || pos > boardSize) throw new ArgumentOutOfRangeException(nameof(pos));
+            Position = pos;
+            State = PlayerState.Playing;
+        }
+        
+        public virtual int Move(int delta, int boardSize)
+        {
+            if (boardSize <= 0) throw new ArgumentException("boardSize must be positive");
+            if (!Position.HasValue) throw new InvalidOperationException("Player not in game");
+
+            DistanceTraveled += Math.Abs((long)delta);
+
+            long baseIndex = (Position.Value - 1) + delta;
+            long mod = ((baseIndex % boardSize) + boardSize) % boardSize;
+            int newPos = (int)(mod + 11);
+            Position = newPos;
+            return newPos;
+        }
+        
         
     }
 }
