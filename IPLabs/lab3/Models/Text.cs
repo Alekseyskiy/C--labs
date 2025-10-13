50fae8c108f5a85e15b728676a96f1901e3f7cb9
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-10-13 18:07:52 +0300

    add some methods to Text.cs

--- IPLabs/lab3/Models/Text.cs
+++ IPLabs/lab3/Models/Text.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace IPLabs.lab3.Models
 {
     public class Text
     {
-        
+        public List<Sentence> Sentences { get; } = new();
+
+        public IEnumerable<Sentence> GetQuestionSentencesWithWordCount(int count) =>
+            Sentences.Where(s => s.IsQuestion && s.WordCount == count);
+
+        public IEnumerable<Sentence> GetSentencesOrderedByLength() =>
+            Sentences.OrderBy(s => s.DisplayLength);
     }
 }
