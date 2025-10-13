c7e0093c4f2ee98a5fce8748a46f94fb6fb090b8
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-10-13 23:29:12 +0300

    implement text parser

--- IPLabs/lab3/Services/TextParser.cs
+++ IPLabs/lab3/Services/TextParser.cs
@@ -1,12 +1,33 @@
+using System.Text.RegularExpressions;
 using IPLabs.lab3.Models;
 
 namespace IPLabs.lab3.Services
 {
     public class TextParser
     {
-        public Text parse(string input)
+        public Text Parse(string input)
         {
-            return new Text();
+            var text = new Text();
+
+            string[] rawSentences = Regex.Split(input, @"(?<=[.!?])\s+");
+
+            foreach (string raw in rawSentences)
+            {
+                var sentence = new Sentence();
+                var tokens = Regex.Matches(raw, @"\w+|[^\w\s]+");
+
+                foreach (Match m in tokens)
+                {
+                    if (Regex.IsMatch(m.Value, @"^\w+$"))
+                        sentence.Tokens.Add(new Word(m.Value));
+                    else
+                        sentence.Tokens.Add(new Punctuation(m.Value));
+                }
+
+                text.Sentences.Add(sentence);
+            }
+
+            return text;
         }
     }
 }
