5a387985fad6c1780d3ee4c1bb0e4884bccfbfdc
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-10-13 18:14:24 +0300

    add test text parse method

--- IPLabs/lab3/Services/TextParser.cs
+++ IPLabs/lab3/Services/TextParser.cs
@@ -1,7 +1,12 @@
+using IPLabs.lab3.Models;
+
 namespace IPLabs.lab3.Services
 {
     public class TextParser
     {
-        
+        public Text parse(string input)
+        {
+            return new Text();
+        }
     }
 }
