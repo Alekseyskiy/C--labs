9b2926072f52fb5fb47e8af9f94c30e871baa7a6
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-11 11:08:13 +0300

    del xml Punctuation.cs

--- IPLabs/lab3/Models/Token.cs
+++ IPLabs/lab3/Models/Token.cs
@@ -3,7 +3,6 @@ using System.Xml.Serialization;
 namespace IPLabs.lab3.Models
 {
     [XmlInclude(typeof(Word))]
-    [XmlInclude(typeof(Punctuation))]
     public interface Token
     {
         string Text { get; set; }
