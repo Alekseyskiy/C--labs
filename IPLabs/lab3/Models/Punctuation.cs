cc86271d822b5e29dff235a9dea07290aeea75e9
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-11 11:05:18 +0300

    del xml from Punctuation.cs

--- IPLabs/lab3/Models/Punctuation.cs
+++ IPLabs/lab3/Models/Punctuation.cs
@@ -2,10 +2,8 @@ using System.Xml.Serialization;
 
 namespace IPLabs.lab3.Models
 {
-    [XmlType("Punctuation")]
     public class Punctuation : Token
     {
-        [XmlElement("Text")]
         public string Text { get; set; }
         
         public Punctuation() { }
