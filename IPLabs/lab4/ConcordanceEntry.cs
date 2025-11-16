9138fa851325f8878b88afa2f308bfa39775e304
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-16 20:59:13 +0300

    init ConcordanceEntry.cs

--- IPLabs/lab4/ConcordanceEntry.cs
+++ IPLabs/lab4/ConcordanceEntry.cs
@@ -4,5 +4,11 @@ namespace IPLabs.lab4;
 
 public class ConcordanceEntry
 {
-    
+    public int Count { get; set; }
+    public SortedSet<int> Lines { get; set; } = new SortedSet<int>();
+
+    public override string ToString()
+    {
+        return $"{Count}: {string.Join(" ", Lines)}";
+    }
 }
