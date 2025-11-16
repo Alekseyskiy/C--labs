66b568ec2b26392f648d56f9ed11fdc6e9f4a64e
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-16 23:40:17 +0300

    init regex in TextProcessor.cs

--- IPLabs/lab4/TextProcessor.cs
+++ IPLabs/lab4/TextProcessor.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace IPLabs.lab4;
 
 public class TextProcessor
 {
-    
+    public Dictionary<string, ConcordanceEntry> BuildConcordance(List<string> lines)
+    {
+        var concordance = new Dictionary<string, ConcordanceEntry>();
+        Regex regex = new Regex(@"[A-Za-zА-Яа-яЁё]+", RegexOptions.IgnoreCase);
+        
+
+        return concordance;
+    }
 }
