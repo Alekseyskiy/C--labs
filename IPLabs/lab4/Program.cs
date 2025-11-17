cfadce280b54f62770c855f19f9f9fb494e9471e
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-17 10:07:26 +0300

    init program

--- IPLabs/lab4/Program.cs
+++ IPLabs/lab4/Program.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace IPLabs.lab4;
 
 public class Program
 {
     public static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
         
+        var processor = new TextProcessor();
+
+        List<string> lines = new List<string>();
+
+        Console.WriteLine("Введите текст построчно. Пустая строка — завершение ввода.\n");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+                break;
+
+            lines.Add(input);
+        }
+
+        var concordance = processor.BuildConcordance(lines);
+
+        foreach (var pair in concordance.OrderBy(p => p.Key))
+        {
+            Console.WriteLine($"{pair.Key,-15} {pair.Value}");
+        }
     }
 }
