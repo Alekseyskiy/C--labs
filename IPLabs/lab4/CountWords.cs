59ccae271866e3554cc5a1d80922c6b783a8289b
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-11-18 15:34:05 +0300

    add CountWords.cs

---
+++ IPLabs/lab4/CountWords.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace IPLabs.lab4;
+
+public class CountWords
+{
+    public int Count { get; set; }
+    
+    public SortedSet<int> Lines { get; set; } =  new SortedSet<int>();
+}
