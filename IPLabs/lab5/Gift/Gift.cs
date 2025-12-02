a2d9944a80a486ded13767183174bce57041fc8f
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-12-02 14:59:12 +0300

    init gift class

--- IPLabs/lab5/Gift/Gift.cs
+++ IPLabs/lab5/Gift/Gift.cs
@@ -1,9 +1,27 @@
 using System.Collections.Generic;
+using System.Linq;
 using IPLabs.lab5.Sweets;
 
 namespace IPLabs.lab5.Gift;
 
 public class Gift
 {
-    List<Sweet> sweets;
+    private List<Sweet> sweets = new  List<Sweet>();
+
+    public void AddSweet(Sweet sweet)
+    {
+        sweets.Add(sweet);
+    }
+    
+    public double GetTotalWeight()
+    {
+        return sweets.Sum(s => s.Weight);
+    }
+    
+    public void SortByWeight()
+    {
+        sweets = sweets.OrderBy(s => s.Weight).ToList();
+    }
+    
+    
 }
