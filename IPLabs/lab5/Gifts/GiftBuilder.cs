e9060577a2ce55e5e857e6f7febb6a63ecea7bc8
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-12-02 17:13:31 +0300

    init GiftBuilder.cs

--- IPLabs/lab5/Gifts/GiftBuilder.cs
+++ IPLabs/lab5/Gifts/GiftBuilder.cs
@@ -1,6 +1,25 @@
+using IPLabs.lab5.Sweets;
+
 namespace IPLabs.lab5.Gifts;
 
 public class GiftBuilder
 {
-    
+    private Gift gift = new Gift();
+
+    public GiftBuilder AddCandy(Candy candy)
+    {
+        gift.AddSweet(candy);
+        return this;
+    }
+
+    public GiftBuilder AddCookie(Cookie cookie)
+    {
+        gift.AddSweet(cookie);
+        return this;
+    }
+
+    public Gift Build()
+    {
+        return gift;
+    }
 }
