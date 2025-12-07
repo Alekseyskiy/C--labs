9baafd11069c14fd81e01e42773c7222de2f3ca8
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-12-07 15:26:45 +0300

    add utf-8 encoding

--- IPLabs/lab5/Program.cs
+++ IPLabs/lab5/Program.cs
@@ -9,6 +9,9 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        
         GiftBuilder builder = new GiftBuilder();
         Gift gift = builder.Build();
 
